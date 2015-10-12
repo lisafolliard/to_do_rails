ce8c02fdc4332ccb1ef6cebf1e3d6ce0b70ea157
Lisa Folliard & Shannon Mulloy <student@epicodus.com>
2015-10-12 11:55:00 -0700

    edit and delete tasks from list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,27 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_path(@task.list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
+  end
+
   private
   def task_params
     params.require(:task).permit(:description)
